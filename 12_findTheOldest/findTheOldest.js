49db2d6a5e4dd458fc4584420fb513c32076aab9
Asbjørn <17161494+Asse69@users.noreply.github.com>
2024-03-27 12:19:34 +0100

    Add implementation for 12 of javascript exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.reduce((oldest, current) => {
+        if (!oldest.yearOfDeath) {
+            let today = new Date();
+            oldest["yearOfDeath"] = today.getFullYear();
+        }
+        if (!current.yearOfDeath) {
+            let today = new Date();
+            current["yearOfDeath"] = today.getFullYear();
+        }
+        if (oldest.yearOfDeath - oldest.yearOfBirth > current.yearOfDeath - current.yearOfBirth) {
+            return oldest;
+        } else {
+            return current;
+        }
+    }, arr[0]);
 };
 
 // Do not edit below this line
